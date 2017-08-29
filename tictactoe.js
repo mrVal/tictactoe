926815d969b5739f4e625411e1715d6fa335ab8e
Valeriy <valeriy.bartenev@gmail.com>
2017-08-29 19:26:30 +0300

    Add win and draw check

--- tictactoe.js
+++ tictactoe.js
@@ -9,25 +9,28 @@ const board = [[null, null, null],
                [null, null, null],
                [null, null, null]];
 
+const leftDiagonal = [[0,0], [1,1], [2,2]];
+const rightDiagonal = [[2,0], [1,1], [0,2]];
+
 let currentPlayer = 'x';
 
 let aI = false;
 
 let squaresLeft = 9;
 
-let greetings = '*TICTACTOE* \nHit 1 to play against a dumb-ass AI \nHit 2 for two-player mode\n';
+let greetings = '*TICTACTOE* \nHit 1 to play against a dumb-ass AI \nHit 2 for two-player mode \n';
 
 function startGame() {
+
   rl.question(greetings, function(answer) {
       switch(answer) {
+
         case '1':
           aI = true;
-          drawBoard();
           nextMove();
           break;
 
         case '2':
-          drawBoard();
           nextMove();
           break;
 
@@ -46,12 +49,13 @@ function nextPlayer() {
 
 function drawBoard() {
 
-  let s = (board.join('\n').replace(new RegExp(',','g'),'|'));
-
-  console.log(s);
+  console.log(board.map(row => row.map(e => e || ' ').join('|')).join('\n'));
 }
 
 function nextMove() {
+
+  drawBoard();
+
   rl.question('Player, '+ currentPlayer +' make a move!\n',
               function (answer) {
     let i = 3 - Math.floor((answer - 1) / 3) - 1;
@@ -61,13 +65,21 @@ function nextMove() {
       board[i][j] = currentPlayer;
       squaresLeft -= 1;
 
-      if (aI) {
-        aIMove();
+      checkEndgame(i, j);
+
+      if(aI) {
+        currentPlayer = nextPlayer();
+
+        [aIRow, aICol] = aIMove();
+
+
+        checkEndgame(aIRow, aICol);
+
+        currentPlayer = nextPlayer();
       } else {
         currentPlayer = nextPlayer();
       }
 
-      drawBoard();
       nextMove();
 
     } else {
@@ -104,18 +116,78 @@ function aIMove() {
     let rndJ = Math.floor(Math.random() * board[rndI].length);
 
     if (board[rndI][rndJ] !== 'x' && board[rndI][rndJ] !== 'o') {
-      currentPlayer = nextPlayer();
 
       board[rndI][rndJ] = currentPlayer;
       squaresLeft -= 1;
 
-      currentPlayer = nextPlayer();
-
     } else if (squaresLeft > 0){
         aIMove();
     } else {
-        console.log('Game over!\n');
+
     }
+    return [rndI,rndJ];
+}
+
+function isWin(row, col) {
+
+  let win;
+  let lines = [];
+
+  [leftDiagonal, rightDiagonal].map(function(line) {
+    line.map(function(cell) {
+      if(cell[0] === row && cell[1] === col) {
+        lines.push(line);
+      }
+    })
+  });
+
+  lines.push([0, 1, 2].map(function(c1) {
+    return [row, c1];
+  }));
+
+  lines.push([0, 1, 2].map(function(r1) {
+    return [r1, col];
+  }));
+
+  win = lines.some(function(line) {
+    return line.every(function(cell) {
+      return board[cell[0]][cell[1]] === currentPlayer;
+    });
+  });
+
+  return win;
+}
+
+function isDraw() {
+  let flatBoard = [];
+
+  for(let i = 0; i < board.length; i++) {
+    flatBoard = flatBoard.concat(board[i]);
+  }
+
+  if(!(flatBoard.includes(null))) {
+      return true;
+    } else {
+      return false;
+  }
+}
+
+function checkEndgame(row, col) {
+  if(isWin(row, col)) {
+
+    drawBoard();
+    console.log(`Player ` + `${currentPlayer} wins!\n`);
+    process.exit();
+
+  } else if(isDraw()) {
+
+    drawBoard();
+    console.log("It's a draw!\n");
+    process.exit();
+
+  } else {
+
+  }
 }
 
 startGame();
