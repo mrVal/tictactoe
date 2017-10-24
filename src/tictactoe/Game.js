fc06df923eff319ed4c00fc7a68274b0c36f4834
Valeriy <valeriy.bartenev@gmail.com>
2017-10-24 14:19:26 +0300

    Refactor Game class

--- src/tictactoe/Game.js
+++ src/tictactoe/Game.js
@@ -3,81 +3,72 @@ const prompt = require('syncprompt');
 module.exports = class Game {
 
   constructor() {
-
     this.board = null;
 
     this.currentPlayer = 'x';
 
     this.aI = false;
 
-    this.squaresLeft;
-
-    this.board = this.getEmptyBoard();
     this.squaresLeft = 9;
+
+    this.board = this._getEmptyBoard();
+  }
+
+  play(useAI) {
+
+    while(true) {
+      this._drawBoard();
+      const [row, col] = this._playerMove();
+
+      if(!this._isLegalMove(row, col)) continue;
+
+        this.board[row][col] = this.currentPlayer;
+        this.squaresLeft -= 1;
+
+      if(this._isGameOver(row, col)) break;
+
+      this._swapPlayer();
+
+      if(useAI) {
+        const [aIRow, aICol] = this._getAIMove();
+
+        if(this._isGameOver(aIRow, aICol)) break;
+
+        this._swapPlayer()
+      }
+    }
 
   }
 
-  getEmptyBoard() {
+  _getEmptyBoard() {
     //use numpad to fill in the board
     return [[null, null, null],   //[7,8,9]
             [null, null, null],   //[4,5,6]
             [null, null, null]];  //[1,2,3]
   }
 
-  _nextPlayer() {
+  _swapPlayer() {
     (this.currentPlayer === 'x') ? this.currentPlayer = 'o'
                                  : this.currentPlayer = 'x';
-
-    return this.currentPlayer;
   }
 
   _drawBoard() {
-
     console.log(this.board.map(row => row.map(e => e || ' ').join('|'))
                                                             .join('\n'));
   }
 
-  play(useAI) {
-
-    while(true) {
-
-      this._drawBoard();
-
-      const [row, col] = this._playerMove();
-
-      if(!this._checkLegalMove(row, col)) continue;
-
-        this.board[row][col] = this.currentPlayer;
-        this.squaresLeft -= 1;
-
-      if(this._checkEndgame(row, col)) break;
-
-      this.currentPlayer = this._nextPlayer();
-
-      if(useAI) {
-        const [aIRow, aICol] = this._getAIMove();
-
-        if(this._checkEndgame(aIRow, aICol)) break;
-
-      this.currentPlayer = this._nextPlayer();
-
-      }
-    }
-  }
-
   _playerMove() {
-
     let answer = prompt('Player, '+ this.currentPlayer +' make a move!\n');
-
     let i = 3 - Math.floor((answer - 1) / 3) - 1;
     let j = (answer - 1) % 3;
 
     return [i,j];
   }
 
-  _checkLegalMove(row, col) {
+  _isLegalMove(row, col) {
 
     if(isFinite(row) && isFinite(col)) {
+
       if((row < 0 || row >= this.board.length) ||
         (col < 0 || col >= this.board[row].length)) {
         console.log('Out of bounds, try another position\n');
@@ -87,36 +78,30 @@ module.exports = class Game {
 
       if(this.board[row][col]) {
         console.log('Cell occupied, try another position\n');
-
         return false;
       }
+
       return true;
     }
-    console.log("Invalid character. Try numbers between 1 and 9.\n")
+    console.log("Invalid character. Try numbers between 1 and 9.\n");
     return false;
   }
 
   _getAIMove() {
-
     let rndI = Math.floor(Math.random() * this.board.length);
-
     let rndJ = Math.floor(Math.random() * this.board[rndI].length);
 
-    if (this.board[rndI][rndJ] !== 'x' && this.board[rndI][rndJ] !== 'o') {
-
+    if(this.board[rndI][rndJ] !== 'x' && this.board[rndI][rndJ] !== 'o') {
       this.board[rndI][rndJ] = this.currentPlayer;
       this.squaresLeft -= 1;
-
     } else if (this.squaresLeft > 0){
-        this._getAIMove();
-    } else {
-
+      this._getAIMove();
     }
+
     return [rndI,rndJ];
   }
 
   _isWin(row, col) {
-
     let win;
     let lines = [];
     let board = this.board;
@@ -126,9 +111,11 @@ module.exports = class Game {
 
     [leftDiagonal, rightDiagonal].map(function(line) {
       line.map(function(cell) {
+
         if(cell[0] === row && cell[1] === col) {
           lines.push(line);
         }
+
       })
     });
 
@@ -156,28 +143,19 @@ module.exports = class Game {
       flatBoard = flatBoard.concat(this.board[i]);
     }
 
-    if(!(flatBoard.includes(null))) {
-        return true;
-      } else {
-        return false;
-    }
+    return !flatBoard.includes(null);
   }
 
-  _checkEndgame(row, col) {
-    if(this._isWin(row, col)) {
+  _isGameOver(row, col) {
 
+    if(this._isWin(row, col)) {
       this._drawBoard();
       console.log(`Player ` + `${this.currentPlayer} wins!\n`);
       return true;
-
     } else if(this._isDraw()) {
-
       this._drawBoard();
       console.log("It's a draw!\n");
       return true;
-
-    } else {
-
     }
   }
 }
