9582f7972ad844652c5da112d8e9e7ce6d9a59a2
Valeriy <valeriy.bartenev@gmail.com>
2017-11-24 19:59:01 +0200

    Refactor code

--- src/tictactoe/Board.js
+++ src/tictactoe/Board.js
@@ -3,86 +3,30 @@ const BoardInvalidRequest = require('./BoardInvalidRequest');
 module.exports = class Board{
 
   constructor () {
-    this._matrix = [[null, null, null],
-                    [null, null, null],
-                    [null, null, null]];
-  }
-
-  _transformToMatrixCoord(position){
-    let i = 3 - Math.floor((position - 1) / 3) - 1;
-    let j = (position - 1) % 3;
-
-    return [i,j];
-  }
-
-  _isValidMark(mark) {
-    return ((mark ==='x') || (mark === 'o'));
-  }
-
-  _isBoardPosition(row, col) {
-    if((Number.isInteger(row) && Number.isInteger(col)) &&
-       (row >= 0 && row < this._matrix.length) &&
-       (col >= 0 && col < this._matrix[row].length)) {
-      return true;
-    }
-
-    return false;
-  }
-
-  _isOccupied(row, col) {
-    return (this._matrix[row][col]);
-  }
-
-  _getFlattenedMatrix() {
-    return this._matrix.reduce(function(acc, curr) {
-      return acc.concat(curr);
-    });
-  }
-
-  _getColumn(col) {
-    let column = [];
-
-    this._matrix.map(function(e){
-      column = column.concat(e[col]);
-    });
-
-    return column;
-  }
-
-  _getRightDiagonal(){
-    let rDiag = [];
-    rDiag.push(this._matrix[2][0]);
-    rDiag.push(this._matrix[1][1]);
-    rDiag.push(this._matrix[0][2]);
-    return rDiag;
-  }
-
-  _getLeftDiagonal(){
-    let lDiag = [];
-    lDiag.push(this._matrix[0][0]);
-    lDiag.push(this._matrix[1][1]);
-    lDiag.push(this._matrix[2][2]);
-    return lDiag;
+    //use numpad to fill in the board
+    this._matrix = [[null, null, null], // [7, 8, 9]
+                    [null, null, null], // [4, 5, 6]
+                    [null, null, null]];// [1, 2, 3]
   }
 
   getMark(row, col) {
     if(!this._isBoardPosition(row, col))
-      throw new BoardInvalidRequest('Out of board range\n');
+      throw new BoardInvalidRequest('Out of board range');
 
     return this._matrix[row][col];
   }
 
   setBoardPosition(position, mark) {
     if(!this._isValidMark(mark))
-      throw new BoardInvalidRequest('Invalid Mark\n');
+      throw new BoardInvalidRequest('Invalid Mark');
 
     const [row, col] = this._transformToMatrixCoord(position);
 
     if(!this._isBoardPosition(row, col))
-      throw new BoardInvalidRequest('Out of board range\n');
+      throw new BoardInvalidRequest('Out of board range');
 
     if(this._isOccupied(row, col))
-      throw new BoardInvalidRequest("Position is occupied\n");
+      throw new BoardInvalidRequest("Position is occupied");
 
     this._matrix[row][col] = mark;
   }
@@ -117,5 +61,53 @@ module.exports = class Board{
     winLines.push(this._getColumn(2));
 
     return winLines;
+  }
+
+  _transformToMatrixCoord(position){
+    let row = 3 - Math.floor((position - 1) / 3) - 1;
+    let col = (position - 1) % 3;
+
+    return [row,col];
+  }
+
+  _isValidMark(mark) {
+    return mark ==='x' || mark === 'o';
+  }
+
+  _isBoardPosition(row, col) {
+    return Number.isInteger(row) && (row >= 0 && row < this._matrix.length) &&
+           Number.isInteger(col) && (col >= 0 && col < this._matrix[row].length)
+  }
+
+  _isOccupied(row, col) {
+    return !!this._matrix[row][col];
+  }
+
+  _getFlattenedMatrix() {
+    return this._matrix.reduce((acc, curr) => acc.concat(curr));
+  }
+
+  _getColumn(col) {
+    let column = [];
+
+    this._matrix.map(e => column = column.concat(e[col]));
+
+    return column;
+  }
+
+  _getRightDiagonal() {
+    let rDiag = [];
+    rDiag.push(this._matrix[2][0]);
+    rDiag.push(this._matrix[1][1]);
+    rDiag.push(this._matrix[0][2]);
+    return rDiag;
+  }
+
+  _getLeftDiagonal() {
+    let lDiag = [];
+    lDiag.push(this._matrix[0][0]);
+    lDiag.push(this._matrix[1][1]);
+    lDiag.push(this._matrix[2][2]);
+    return lDiag;
   }
 }
